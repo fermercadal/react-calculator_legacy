c44bcf2432ad5b8abfa40f16731040e2a751085e
Fernando Mercadal <fermercadal@outlook.com>
2020-05-13 13:17:57 -0300

    Update operations

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
-// up to 1:09:12
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      lastPressed: undefined,
+      lastPressed: '',
       currentNumber: '0',
-      previousNumber: undefined,
-      operation: undefined
+      previousNumber: '',
+      operation: ''
     }
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick = (e) => {
+    const lastPressed = this.state.lastPressed;
     const currentNumber = this.state.currentNumber;
     const previousNumber = this.state.previousNumber;
     const operation = this.state.operation
     const value = e.target.innerText;
     
-    
     if(!Number.isNaN(Number(value))) {
-      if (currentNumber === '0') {
+      if(lastPressed === '=' && operation === '=') {
         this.setState({
-          currentNumber : value
+          currentNumber : value,
+          operation: '',
+          previousNumber: ''
         });
       } else {
-        this.setState({
-          currentNumber : currentNumber + value
-        });
+        if (currentNumber === '0') {
+          this.setState({
+            currentNumber : value
+          });
+        } else {
+          this.setState({
+            currentNumber : currentNumber + value
+          });
+        }
       }
 
       return
@@ -41,8 +48,8 @@ class App extends Component {
       case('AC') : {
         this.setState({
           currentNumber: '0',
-          previousNumber: undefined,
-          operation: undefined
+          previousNumber: '',
+          operation: ''
         });
         break
       }
@@ -52,6 +59,7 @@ class App extends Component {
             currentNumber: currentNumber + value
           });
         }
+        break
       }
       default: {
         if(!operation) {
@@ -60,15 +68,31 @@ class App extends Component {
             previousNumber: currentNumber,
             currentNumber: '0'
           });
-        } else {
+        } else if(lastPressed === '=' && operation === '=') {
+          if(value !== '=') {
+            this.setState({
+              operation: value,
+              previousNumber: currentNumber,
+              currentNumber: '0'
+            });
+          } else {
+            return false;
+          }
+        } /*else if (lastPressed === '/' || lastPressed === '*') {
+          if(value === '-') {
+            this.setState({
+              currentNumber: value
+            });
+            return false
+          }
+        } */else {
           const evaluated = eval(`${previousNumber} ${operation}  ${currentNumber}`)
+
           this.setState({
             operation: value,
-            previousNumber: evaluated,
+            previousNumber: `${previousNumber} ${operation}  ${currentNumber}`,
             currentNumber: value === '=' ? evaluated : '0'
           });
-
-          
         }
       }
     }
@@ -79,32 +103,120 @@ class App extends Component {
   }
 
   render() {
-    const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
-    const operations = ['/', '*', '-', '+', '='];
+    const numbers = [
+      {
+        value: 7,
+        id: 'seven'
+      },
+      {
+        value: 8,
+        id: 'eight'
+      },
+      {
+        value: 9,
+        id: 'nine'
+      },
+      {
+        value: 4,
+        id: 'four'
+      },
+      {
+        value: 5,
+        id: 'five'
+      },
+      {
+        value: 6,
+        id: 'six'
+      },
+      {
+        value: 1,
+        id: 'one'
+      },
+      {
+        value: 2,
+        id: 'two'
+      },
+      {
+        value: 3,
+        id: 'three'
+      },
+      {
+        value: 0,
+        id: 'zero'
+      }
+    ];
+    const operations = [
+      {
+        value: '/',
+        id: 'divide'
+      }, 
+      {
+        value: '*',
+        id: 'multiply'
+      }, 
+      {
+        value: '-',
+        id: 'subtract'
+      }, 
+      {
+        value: '+',
+        id: 'add'
+      }, 
+      {
+        value: '=',
+        id: 'equals'
+      }
+    ];
+
+    const previousNumber = this.state.previousNumber;
+    const operation = this.state.operation;
     const currentNumber = this.state.currentNumber;
+    const currentOperation = currentNumber === '0' && previousNumber === '' ? '' : `${ previousNumber } ${ operation }  ${ currentNumber }`;
 
     return (
       <main id="calculator" className="calculator">
+      <p style={{position: 'absolute', top: 0}}>
+        { JSON.stringify(this.state, null, 2) }
+      </p>
        
-        <div className="display">
-          {currentNumber}
+        <div className="currentOperation">
+          { currentOperation }
+        </div>
+        <div className="display" id="display">
+          { currentNumber }
         </div>
 
-        <button className="ac" onClick={this.handleClick}>AC</button>
+        <button 
+          className="ac" 
+          id="clear"
+          onClick={this.handleClick}>
+            AC
+        </button>
 
-        {numbers.map(number => (
-          <button key={number} onClick={this.handleClick}>
-            {number}
+        { numbers.map(number => (
+          <button 
+            key={ number.value }
+            id={ number.id }
+            onClick={this.handleClick}>
+            { number.value }
           </button>
-        ))}
+        )) }
 
-        <button className="dot" onClick={this.handleClick}>.</button>
+        <button 
+          className="dot"
+          id="decimal"
+          onClick={this.handleClick}>
+            .
+        </button>
 
-        {operations.map(operation => (
-          <button key={operation} onClick={this.handleClick}>
-            {operation}
+        { operations.map(operation => (
+          <button 
+            key={ operation.id } 
+            id={ operation.id } 
+            onClick={this.handleClick}>
+              { operation.value }
           </button>
-        ))}
+        )) }
    
       </main>
     );
