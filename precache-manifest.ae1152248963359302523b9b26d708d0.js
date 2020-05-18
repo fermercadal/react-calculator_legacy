2f02759b0f745a5b6b7a64d2363f4a826a06f7c3
Fernando Mercadal <fermercadal@outlook.com>
2020-05-18 00:15:30 -0300

    Updates

---
+++ precache-manifest.ae1152248963359302523b9b26d708d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e5f2de8036ab36d12da83bd497793e",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "156bde687060738b770b",
+    "url": "/react-calculator/static/css/main.05ecd29c.chunk.css"
+  },
+  {
+    "revision": "499602df903a993cf292",
+    "url": "/react-calculator/static/js/2.0b26af30.chunk.js"
+  },
+  {
+    "revision": "156bde687060738b770b",
+    "url": "/react-calculator/static/js/main.c7e7e0dd.chunk.js"
+  },
+  {
+    "revision": "c8cd6608ef516393abcd",
+    "url": "/react-calculator/static/js/runtime-main.b54989df.js"
+  }
+]);
